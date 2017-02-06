d48a35aad66d2606dc59df31bcce60ffe82e26dc
Victorien <fb@vito.fr.nf>
2017-02-06 21:23:02 +0100

    Gestion des petit MP sans pagination

--- jvc-mp-plus.user.js
+++ jvc-mp-plus.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JVC MP Plus
 // @namespace    https://github.com/vitoo
-// @version      0.2
+// @version      0.3
 // @description  Ajout de fonctionnalité aux MP
 // @author       vitoo
 // @match        http://www.jeuxvideo.com/messages*
@@ -14,23 +14,43 @@
 
 
 var url = jQuery('a.btn-25-msg').attr('href');
-jQuery('a.btn-25-msg').text('< 25 messages');
+if(!url){ //si le bouton de pagination est présent
+    return false;
+}
+var text = jQuery('a.btn-25-msg').text();
+var nbr_message  = parseInt(text.replace ( /[^\d.]/g, '' ));
+if(nbr_message < 5){ //si on a au moins 5 messages dans le bouton de pagination
+    return false;
+}
 jQuery('a.btn-25-msg').remove();
+
 var regexp = /(.*)?id=([0-9]*)&offset=([0-9]*)(.*)/i;
 var result = url.match(regexp);
-var paginations = [5, 10, 25, 50];
 var id_mp = result[2];
-var offset = parseInt(result[3]);
+var offset = parseInt(result[3]); 
+var current_position = offset+nbr_message;  //nombre de message affiché dans le MP 
+
+var paginations = [5, 10, 25, 50];
+//remove useless pagination
+for(i = 0 ; i < paginations.length  ; i++){
+    if(paginations[i] >= current_position){
+        paginations.splice(i, 4); 
+    }
+}
+
 paginations.forEach(function (element, idx, array) {
     if (idx === array.length - 1) {
-        //dernier bouton on affiche 'message'
+        //si dernier bouton on affiche 'messages'
         var message = " messages";
     }
     else {
         var message = "";
     }
-    //if offset > element :
-    var offset_loop = offset + (25) - element;
+    var offset_loop = current_position - element;
     var html = '<a href="/messages-prives/message.php?id=' + id_mp + '&amp;offset=' + offset_loop + '&amp;folder=1" class="btn btn-25-msg" sl-processed="1">< ' + element + message + ' </a>&nbsp;';
     jQuery('.pagination > div.action-center').append(html);
+
 });
+//debug
+//console.log("nbr_message "  + nbr_message);
+//console.log("current_position : " + current_position);
